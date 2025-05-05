d32592f145446d968f2256efffc0b214f1c846a8
sazh <kevin.loulendo@hotmail.fr>
2025-05-05 08:13:59 +0200

    pour soutenance

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     return (
         <nav className="navbar">
             <div className="navbar__logo">
-                <img src="/logo.svg" alt="logo du site Kasa" />
+            <NavLink to="/"><img src="/logo.svg" alt="logo du site Kasa" /></NavLink>
             </div>
             <NavLink to="/">Accueil</NavLink>
             <NavLink to="/about">A propos</NavLink>
